e1d9be632524847051abf48b2d8cd9365f336190
luizavramos <luizavramos30@gmail.com>
2022-07-11 08:57:39 -0300

    Ajuste no exercicio 08/07

--- 2022.07.08/2022.07.08/ListaExerciciosLoop/src/Exercicios/ExercicioDoisIf.java
+++ 2022.07.08/2022.07.08/ListaExerciciosLoop/src/Exercicios/ExercicioDoisIf.java
@@ -1,23 +1,46 @@
 package Exercicios;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class ExercicioDoisIf {
 
 	public static void main(String[] args) {
+		
+		
 		Scanner entrada = new Scanner(System.in);
 		
-		int aux;
+		int aux, aux2;
 		
 		int [] listaDeNumeros = new int[3];
+		
 		for(int i = 0; i < 3; i++) {
 			aux = ( i + 1 );
-			System.out.println("Digite o " + aux + "º número: ");
+			System.out.println("Digite o " + aux + "Âº nÃºmero: ");
 			listaDeNumeros[i] = (entrada.nextInt());
-			}
+			if (listaDeNumeros[0] < listaDeNumeros[i]){
+				aux2 = listaDeNumeros[0];
+				listaDeNumeros[0] = listaDeNumeros[i];
+				listaDeNumeros[i] = aux2;
+			} 
+		}
 		
-		Arrays.sort(listaDeNumeros);
+		for(int i = 0; i < 3; i++) {
+			if (listaDeNumeros[1] < listaDeNumeros[i]){
+				aux2 = listaDeNumeros[1];
+				listaDeNumeros[1] = listaDeNumeros[i];
+				listaDeNumeros[i] = aux2;
+			} 
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			if (listaDeNumeros[2] < listaDeNumeros[i]){
+				aux2 = listaDeNumeros[2];
+				listaDeNumeros[2] = listaDeNumeros[i];
+				listaDeNumeros[i] = aux2;
+			} 
+		}		
+		
 		
 		System.out.println("Ordem crescente:  ");
 		for (int j = 0; j < 3; j++) {
